92d9fcf7d86cf8c3867b907d3e8664b679eb468a
MarineHuang <huang_yx005@163.com>
2023-08-07 21:18:55 +0800

    排除ST股，排除无涨跌幅限制的股票

--- strategy/keep_increasing.py
+++ strategy/keep_increasing.py
@@ -5,12 +5,15 @@ import pandas as pd
 import logging
 
 
-# 持续上涨（MA30向上）
-def check(code_name, data, end_date=None, threshold=30):
+# 均线多头排列（MA5, MA10, MA20, MA120向上）
+def check(code_name, data, end_date=None, threshold=120):
     if len(data) < threshold:
         logging.debug("{0}:样本小于{1}天...\n".format(code_name, threshold))
         return
-    data['ma30'] = pd.Series(tl.MA(data['收盘'].values, 30), index=data.index.values)
+    data['ma5'] = pd.Series(tl.MA(data['收盘'].values, 5), index=data.index.values)
+    data['ma10'] = pd.Series(tl.MA(data['收盘'].values, 10), index=data.index.values)
+    data['ma20'] = pd.Series(tl.MA(data['收盘'].values, 20), index=data.index.values)
+    data['ma120'] = pd.Series(tl.MA(data['收盘'].values, 120), index=data.index.values)
 
     if end_date is not None:
         mask = (data['日期'] <= end_date)
@@ -18,12 +21,21 @@ def check(code_name, data, end_date=None, threshold=30):
 
     data = data.tail(n=threshold)
 
-    step1 = round(threshold/3)
-    step2 = round(threshold*2/3)
-
-    if data.iloc[0]['ma30'] < data.iloc[step1]['ma30'] < \
-        data.iloc[step2]['ma30'] < data.iloc[-1]['ma30'] and data.iloc[-1]['ma30'] > 1.2*data.iloc[0]['ma30']:
+    #if data.iloc[0]['ma30'] < data.iloc[step1]['ma30'] < \
+    #    data.iloc[step2]['ma30'] < data.iloc[-1]['ma30'] and data.iloc[-1]['ma30'] > 1.2*data.iloc[0]['ma30']:
+    
+    
+    # 在最近的3个交易日均线均多头排列
+    if (data.iloc[0]['ma5']  >= data.iloc[0]['ma10']
+        and data.iloc[0]['ma10'] >= data.iloc[0]['ma20']
+        and data.iloc[0]['ma20'] >= data.iloc[0]['ma120']
+        #and data.iloc[1]['ma5']  >= data.iloc[1]['ma10']
+        #and data.iloc[1]['ma10'] >= data.iloc[1]['ma20']
+        #and data.iloc[1]['ma20'] >= data.iloc[1]['ma120']
+        #and data.iloc[2]['ma5']  >= data.iloc[2]['ma10']
+        #and data.iloc[2]['ma10'] >= data.iloc[2]['ma20']
+        #and data.iloc[2]['ma20'] >= data.iloc[2]['ma120']
+    ):
         return True
     else:
         return False
-
--- work_flow.py
+++ work_flow.py
@@ -21,6 +21,9 @@ def prepare():
     logging.info("************************ process start ***************************************")
     all_data = ak.stock_zh_a_spot_em()
     subset = all_data[['代码', '名称']]
+
+    subset = subset[subset['代码'].str.startswith(('600', '601', '603', '605', '000', '002'))]
+    subset = subset[~subset['名称'].str.contains('ST')]
     stocks = [tuple(x) for x in subset.values]
     statistics(all_data, stocks)
 
@@ -28,27 +31,35 @@ def prepare():
         '放量上涨': enter.check_volume,
         '均线多头': keep_increasing.check,
         '停机坪': parking_apron.check,
-        '回踩年线': backtrace_ma250.check,
-        # '突破平台': breakthrough_platform.check,
+        #'回踩年线': backtrace_ma250.check,
+        '突破平台': breakthrough_platform.check,
         '无大幅回撤': low_backtrace_increase.check,
-        '海龟交易法则': turtle_trade.check_enter,
+        #'海龟交易法则': turtle_trade.check_enter,
         '高而窄的旗形': high_tight_flag.check,
-        '放量跌停': climax_limitdown.check,
+        #'放量跌停': climax_limitdown.check,
     }
 
     if datetime.datetime.now().weekday() == 0:
         strategies['均线多头'] = keep_increasing.check
 
-    process(stocks, strategies)
+    strategies_result = process(stocks, strategies)
 
+    set1= strategies_result['放量上涨'].intersection(strategies_result['均线多头'])
+    logging.info(f"放量上涨 & 均线多头: \n {set1}")
+    set2= strategies_result['突破平台'].intersection(strategies_result['均线多头'])
+    logging.info(f"突破平台 & 均线多头: \n {set2}")
 
     logging.info("************************ process   end ***************************************")
 
 def process(stocks, strategies):
     stocks_data = data_fetcher.run(stocks)
+    strategies_result={}
     for strategy, strategy_func in strategies.items():
-        check(stocks_data, strategy, strategy_func)
+        results = check(stocks_data, strategy, strategy_func)
+        strategies_result[strategy] = results
         time.sleep(2)
+
+    return strategies_result
 
 def check(stocks_data, strategy, strategy_func):
     end = settings.config['end_date']
@@ -56,7 +67,8 @@ def check(stocks_data, strategy, strategy_func):
     results = dict(filter(m_filter, stocks_data.items()))
     if len(results) > 0:
         push.strategy('**************"{0}"**************\n{1}\n**************"{0}"**************\n'.format(strategy, list(results.keys())))
-
+    
+    return set(results.keys())
 
 def check_enter(end_date=None, strategy_fun=enter.check_volume):
     def end_date_filter(stock_data):
